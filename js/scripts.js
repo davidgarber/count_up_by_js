316abc837c8210dd224bd1122277271ebc27c185
David Garber & Jennifer Bolanos <student@epicodus.com>
2015-05-28 10:44:21 -0700

    count up to a number in multiples of a specified number.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,22 @@
-var countTo = function(x) {
-  for(x = 1; x <= 3; x++)
-  return x;
+// var countTo = function(x) {
+//   for(x = 1; x <= 3; x++)
+//   return x;
+// }
+//
+// var countBy = function(y) {
+// var numbers = [2,4,6]
+//   for(y = countBy; y <= countTo; y++)
+//     if(countBy % y === 0);
+//       return y;
+// }
+
+var countUpby = function(multipleOf, count) {
+var x = multipleOf
+var y =  count
+var result = []
+  for(var i=x ; i <= count; i+=x) {
+      result.push(i);
+  }
+  return result;
+
 }
--- spec/specs.js
+++ spec/specs.js
@@ -1,11 +1,17 @@
-describe("countTo", function() {
-  it("will count in numerical order by 1 to a number", function() {
-      expect(countTo(3)).to.equal(1,2,3);
-  });
-});
+// describe("countTo", function() {
+//   it("will count in numerical order by 1 to a number", function() {
+//       expect(countTo(3)).to.equal(1,2,3);
+//   });
+// });
+//
+// describe("countBy", function() {
+//   it("will count in multiples of a specified number", function() {
+//     expect(countBy(2)).to.equal(2,4,6);
+//   });
+// });
 
-describe("countBy", function() {
-  it("will count in multiples of a specified number", function() {
-    expect(countBy(2)).to.equal(2,4,6);
+describe("countUpby", function() {
+  it("will count up to a number in multiples of a specified number", function() {
+    expect(countUpby(2,6)).to.eql([2,4,6]);
   });
 });
